41dfacd0a7988bf638a8d6e9a2c791e2727ee8a3
Abram2k1 <smallblueidea@gmail.com>
2020-06-02 14:52:26 -0700

    Commit All Files Javascript Included

---
+++ login-page.js
@@ -0,0 +1,20 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "admin" && password === "Youknowit1234") {
+        alert("You have successfully logged in.");
+        //location.reload();
+        window.location.replace("/Users/Abram/Desktop/TEST/LiefWay.html");
+       // location.href("/LiefWay.html");
+        return true
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
